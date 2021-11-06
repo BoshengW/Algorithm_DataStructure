1bcfe59ce7be4b67f69254a7eefd66aafeb4cdb7
Bosheng <boshengwu1994@gmail.com>
2021-11-06 11:08:52 -0400

    add two pointer same direction

--- src/Basic_Algorithm/twopointer/sameDirection/MoveToZero.java
+++ src/Basic_Algorithm/twopointer/sameDirection/MoveToZero.java
@@ -5,6 +5,11 @@ public class MoveToZero {
     * Lintcode 539 move to zero question
     *
     * */
+    private int[] test() {
+        return new int[]{-1,-1};
+    }
+    public static void main(String[] args) {
 
+    }
 
 }
